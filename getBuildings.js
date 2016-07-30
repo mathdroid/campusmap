811f379db6ddb837884eaa522452f8d2441ad5d9
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2016-07-30 17:15:17 +0700

    Fix dependencies ☺

--- getBuildings.js
+++ getBuildings.js
@@ -2,20 +2,20 @@ const tj = require('togeojson')
 const fs = require('fs')
 const jsdom = require('jsdom').jsdom
 
-try {
-  fs.statSync('./config.json').isFile() ? console.log(chalk.green('✓') + ' Config file found.') : process.exit()
-  let data = fs.readFileSync('./config.json')
-  try {
-    config = JSON.parse(data)
-  // console.dir(config)
-  } catch (err) {
-    console.log(chalk.red('✗') + ' There has been an error parsing /config.json.')
-    config = {}
-  }
-} catch (err) {
-  console.log(chalk.red('✗') + ' No configuration file found.')
-  config = {}
-}
+// try {
+//   fs.statSync('./config.json').isFile() ? console.log(chalk.green('✓') + ' Config file found.') : process.exit()
+//   let data = fs.readFileSync('./config.json')
+//   try {
+//     config = JSON.parse(data)
+//   // console.dir(config)
+//   } catch (err) {
+//     console.log(chalk.red('✗') + ' There has been an error parsing /config.json.')
+//     config = {}
+//   }
+// } catch (err) {
+//   console.log(chalk.red('✗') + ' No configuration file found.')
+//   config = {}
+// }
 
 
 let kml = jsdom(fs.readFileSync('./campus.kml', 'utf8'))
