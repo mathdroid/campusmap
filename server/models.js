105e92f95a5cdaf9b6ef23ee6c80ea74253ab516
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2016-08-12 17:25:26 +0700

    update models

--- server/models.js
+++ server/models.js
@@ -16,14 +16,56 @@ exports.Room = mongoose.model('rooms', new mongoose.Schema({
 }))
 
 exports.Floor = mongoose.model('floors', new mongoose.Schema({
+  id: String,
+  buildingId: String,
+  buildingName: String,
+  rooms: [{
+    name: String,
+    building: String,
+    lat: Number,
+    lon: Number,
+    alt: Number,
+    heading: Number,
+    tilt: Number,
+    range: Number,
+    buildingId: String,
+    floorId: String,
+    gId: String
+  }]
 
 }))
 exports.Building = mongoose.model('buildings', new mongoose.Schema({
-
+  id: String,
+  name: String,
+  floors: [{
+    id: String,
+    rooms: [{
+      gId: String
+    }]
+  }]
 }))
 exports.FloorPolygon = mongoose.model('floorPolygons', new mongoose.Schema({
+  floorId: String,
+  buildingId: String,
+  polygons: [{
+    type: String,
+    name: String,
+    buildingName: String,
+    buildingId: String,
+    floorId: String,
+    coordinates: [[Number]]
+  }]
 
 }))
 exports.VRoomPolygon = mongoose.model('vRoomPolygons', new mongoose.Schema({
+  type: String,
+  name: String,
+  roomName: String,
+  buildingName: String,
+  buildingId: String,
+  floorId: String,
+  gId: String,
+  roomId: String,
+  coordinates: [[ Number ]]
 
 }))
